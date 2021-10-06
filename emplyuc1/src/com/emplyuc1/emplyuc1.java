9506512b977bfe4b5d3194fadba11c4417e5e351
nandinigautam <nandinigautam28@gmail.com>
2021-10-06 19:34:08 +0530

    java

---
+++ emplyuc1/src/com/emplyuc1/emplyuc1.java
@@ -0,0 +1,18 @@
+package com.emplyuc1;
+
+public class emplyuc1 {
+
+	public static void main(String[] args) {
+				int Is_Full_Time =1;
+			double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck ==  Is_Full_Time) {
+			System.out.println(" Employee is present ");
+		}
+		else {
+			System.out.println(" Employee is absent ");
+		}
+		
+	}
+
+
+}
---
+++ empwgUC4/empwgUC4.java
@@ -0,0 +1,30 @@
+package com;
+
+public class empwgUC4 {
+	
+		
+	public static void main(String[] args) {
+		int IS_PART_TIME =1;
+		int IS_FULL_TIME =2;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empwage = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_FULL_TIME )
+			empHrs = 8;
+	  else 
+			empHrs = 4;
+	 
+		empwage = empHrs * EMP_RATE_PER_HOUR; 
+				
+			System.out.println(" Emp wage " + empwage);
+		
+
+
+	}
+
+
+
+	}
+
+
