2f4500ce5a79f19be2909c53712b82d74f078012
NANDINI GAUTAM <89217282+nandinigautamgithub@users.noreply.github.com>
2021-10-06 23:42:18 +0530

    java

---
+++ geometryUC2.java
@@ -0,0 +1,16 @@
+package com.UC2;
+
+public class geometryUC2 {
+
+	public static void main(String[] args) {
+		int x1,x2,y1,y2;
+		x1=1;y1=1;x2=4;y2=4;
+		if (x1 == y1  && x2 == y2) 
+			System.out.println("equal");
+		else {
+			System.out.println("uequal");
+		}
+
+	}
+
+}
---
+++ geometryUC3.java
@@ -0,0 +1,16 @@
+package com.UC3;
+
+public class geometryUC3 {
+
+	public static void main(String[] args) {
+		int x1,x2,y1,y2;
+		x1=2;y1=1;x2=4;y2=5;
+		if (x1 > y1  &&  x2 < y2) 
+			System.out.println("x1 is greater and x2 is smaller");
+		else {
+			System.out.println("y1 is greater and y2 is smaller");
+		}
+
+	}
+
+}
